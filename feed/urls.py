5d7f2447bb0c6920661991118ed4f619f9189ff7
Kevin Alvarez <kevinalvarez1025@gmail.com>
2021-03-03 22:58:38 -0500

    Completed profile page

--- feed/urls.py
+++ feed/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('user/<str:username>', views.profile, name='profile'),
     path('', views.index, name='new-post'),
+    path('', views.index, name='edit-profile'),
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db.models import ObjectDoesNotExist
+from feed.models import Profile
 
 # Create your views here.
 @login_required
@@ -8,4 +11,9 @@ def index(request):
 
 @login_required
 def profile(request,username):
-    return render(request, 'feed/profile.html', {'username':username})
+    try:
+        user = User.objects.get(username=username)
+        profile = Profile.objects.get(user=user)
+    except ObjectDoesNotExist:
+        profile = None
+    return render(request, 'feed/profile.html', {'profile':profile,'username':user.get_username()})
